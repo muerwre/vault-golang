c83116fafa96c94dd50027264c695c31703cf0b1
Fedor Katurov <gotham48@gmail.com>
2020-07-08 22:38:56 +0700

    fixed todos

--- main.go
+++ main.go
@@ -5,17 +5,16 @@ package main
 
 	-- not working ---
 
-	TODO: heroes displaying all nodes
 	TODO: /uploads
 	TODO: /stats
 	TODO: /node POST
 	TODO: youtube titles
 	TODO: update song titles after save
-	TODO: posting comments
 
 	TODO: refactor GetDiff
 	TODO: add Responses and Requests
 
+	DONE: posting comments
 	DONE: updating user profile
 	DONE: updates: new messages not displayed in notifications because they should be Notification type, not message
 */
