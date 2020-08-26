1df8453f650a04dd02fcf42f81727867ede7bfd2
Fedor Katurov <gotham48@gmail.com>
2020-08-26 16:34:32 +0700

    updated todos

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 // https://aaf.engineering/go-web-application-structure-part-2/
+
 /*
+
 	-- before release --
-	TODO: user not updated
 	TODO: add healthcheck endpoint
 	TODO: refactor GetDiff
 
@@ -11,20 +12,6 @@ package main
 	TODO: move all queries to repositories
 	TODO: FRONT remove other user's profile completely, replace it with hover popup and make settings big fullscreen with message dialogs and etc
 
-	DONE: test social login
-	DONE: setup deploy
-	DONE: search
-	DONE: social login (vk, google)
-	DONE: node view not updated? (actually, not getting last_seen_boris property for user)
-	DONE: youtube titles
-	DONE: /node POST
-	DONE: /uploads
-	DONE: /related
-	DONE: add Responses and Requests
-	DONE: /stats
-	DONE: posting comments
-	DONE: updating user profile
-	DONE: updates: new messages not displayed in notifications because they should be Notification type, not message
 */
 
 import (
