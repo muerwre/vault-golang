42c776fa243f52ddccc876f8fb77dbcc78a4d745
muerwre <gotham48@gmail.com>
2020-08-26 20:30:16 +0700

    updating user's last_seen_messages

--- request/user.go
+++ request/user.go
@@ -2,21 +2,23 @@ package request
 
 import (
 	"reflect"
+	"time"
 
 	"github.com/muerwre/vault-golang/models"
 	"github.com/muerwre/vault-golang/utils/passwords"
 )
 
 type UserPatchRequest struct {
-	ID          uint
-	Username    string `json:"username" validate:"omitempty,gte=3,lte=64"`
-	Fullname    string `json:"fullname" validate:"omitempty,gte=1,lte=64"`
-	Password    string `json:"password"`
-	NewPassword string `json:"new_password" validate:"omitempty,gte=6,lte=64"`
-	Email       string `json:"email" validate:"omitempty,gte=2,email"`
-	Description string `json:"description" validate:"omitempty,lte=512"`
-	PhotoID     uint   `json:"-"`
-	CoverID     uint   `json:"-"`
+	ID               uint
+	Username         string     `json:"username" validate:"omitempty,gte=3,lte=64"`
+	Fullname         string     `json:"fullname" validate:"omitempty,gte=1,lte=64"`
+	Password         string     `json:"password"`
+	NewPassword      string     `json:"new_password" validate:"omitempty,gte=6,lte=64"`
+	Email            string     `json:"email" validate:"omitempty,gte=2,email"`
+	Description      string     `json:"description" validate:"omitempty,lte=512"`
+	PhotoID          uint       `json:"-"`
+	CoverID          uint       `json:"-"`
+	LastSeenMessages *time.Time `json:"last_seen_messages"`
 
 	Photo *models.File
 	Cover *models.File
@@ -50,6 +52,10 @@ func (upd *UserPatchRequest) ApplyTo(u *models.User) {
 
 	if upd.PhotoID != 0 {
 		u.PhotoID = &upd.PhotoID
+	}
+
+	if upd.LastSeenMessages != nil {
+		u.LastSeenMessages = *upd.LastSeenMessages
 	}
 }
 
