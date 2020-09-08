a17d7d063ac2f37b58c70e0ee9fa45399d4cf073
Fedor Katurov <gotham48@gmail.com>
2020-09-08 11:13:09 +0700

    updated todo

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ package main
 	Goods:
 	https://awesomeopensource.com/project/FlowerWrong/awesome-gin - list of good things
 	https://github.com/olahol/melody - websocket middleware I'll use
+	https://github.com/mccutchen/palettor - extract palette from image
+
 */
 
 /*
@@ -15,7 +17,6 @@ package main
 	-- after release --
 	TODO: websockets https://github.com/gin-gonic/gin/issues/461
 	TODO: refactor GetDiff
-	TODO: move all queries to repositories
 	TODO: FRONT remove other user's profile completely, replace it with hover popup and make settings big fullscreen with message dialogs and etc
 
 */
